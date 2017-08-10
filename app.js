7613560d33b9b038b55b7c11f84f75eacc273e28
mogaiskii <sani.mog@gmail.com>
2017-08-10 15:02:26 +0700

    EJS

--- app.js
+++ app.js
@@ -1,59 +1,114 @@
 var express = require('express')
   , app = express()
   , port = process.env.PORT || 3000
+  , bodyParser = require('body-parser')
 
 
-//ejs settings
+// ejs settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
+// express settings
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', async function(req,res){
-  res.send('home')
+  res.render('home')
+  //var last_page = DB.get(count posts) / 10 , +1, if дробь
+  //var author = DB.get(select * from users where id= post.author)
+   //{"page":{"title":"Главная", "current":1,"last":last_page},"posts":
+   //  [{"title":"Заголовок поста","id":1,"preview":"Описание поста","author":
+   //   author}]}
 })
 
 app.get('/page/:page', async function(req,res){
-  res.send('home page#'+req.params.page)
+  res.render('home')
+  //var last_page = DB.get(count posts) / 10 , +1, if дробь
+  //var author = DB.get(select * from users where id= post.author)
+   //{"page":{"title":"Главная", "current":req.params.page,"last":last_page},"posts":
+   //  [{"title":"Заголовок поста","id":1,"preview":"Описание поста","author":
+   //   author}]}
 })
 
 app.get('/register', async function(req,res){
-  res.send('registration GET')
+  //if (authorised) res.redirect('/')
+  res.render('register')
+  //{"page":{"title":"Регистрация"}}
 })
 
 app.post('/register', async function(req,res){
-  res.send("registration POST")
+  //if (authorised) res.redirect('/')
+  //checks FORM, if incorrect:
+  res.render('register')
+  //{"errors":[{"title":"Название ошибки", "message":"Описание ошибки"}],
+  //"form":{"email":req.body.email,
+   //  "name":req.body.name,"password":req.body.password,
+   //  "passwordagain":req.body.passwordagain},
+ //"page"{"title":"Регистрация"}}
+})
+
+app.get('/auth', async function(req,res){
+  //if (authorised) res.redirect('/')
+  res.render('auth')
+  //{"page"{"title":"Регистрация"}}
 })
 
 app.post('/auth', async function(req,res){
-  res.send('auth GET')
-})
-
-app.post('/auth', async function(req,res){
-  res.send('auth POST')
+  //if (authorised) res.redirect('/')
+  //checks FORM, if incorrect:
+  res.render('auth')
+  //{"errors":[{"title":"Название ошибки", "message":"Описание ошибки"}]
+  //"form":{"email":req.body.email},
+  // "page":"Вход"}
 })
 
 app.get('/post/:id', async function(req,res){
-  res.send('post#'+req.params.id)
+  //var post = DB(select * from posts where id= req.params.id)
+  res.render('details')
+  //{"page":post.title,"post":post}
 })
 
 app.get('/post/new', async function(req,res){
-  res.send('new post GET')
+  //if (!auth) res.redirect('/auth')
+  res.render('post')
+  //{"page":{"title":"Новый пост"}}
 })
 
 app.post('/post/new', async function(req,res){
-  res.send('new post POST')
+  //if (!auth) res.redirect('/auth')
+  //else if( all ok) DB(insert into posts (title, preview, text)
+  // values (req.body.title,req.body.preview, req.body.text))
+  // res.redirect('/post/'+id)
+  //else (sth wrong)
+  res.render('post')
+  //{"page":{"title":"Новый пост"}, "form":{"title":req.body.title,
+  //  "preview":req.body.preview,"text":req.body.text}}
 })
 
 app.get('/post/:id/edit', async function(req,res){
-  res.send('edit post#'req.params.id' GET')
+  //if (!auth) res.redirect('/auth')
+  res.render('post')
+  //{"page":{"title":"Редактирование"}, "form":{"title":req.body.title,
+  //  "preview":req.body.preview,"text":req.body.text}}
 })
 
 app.put('/post/:id', async function(req,res){
+  //if (!auth) res.redirect('/auth')
+  //else if( all ok) DB(insert into posts (title, preview, text)
+  // values (req.body.title,req.body.preview, req.body.text))
+  // res.redirect('/post/'+id)
+  //else (sth wrong)
   res.send('edit post#'req.params.id' PUT')
+  //{"page":{"title":"Новый пост"}, "form":{"title":req.body.title,
+  //  "preview":req.body.preview,"text":req.body.text}}
 })
 
 app.get('/author/:id', async function(req,res){
-  res.send('author#'+req.params.id)
+  //var user = DB(select * from users where id= req.params.id)
+  //var posts = DB(select * from posts where author = user)
+  res.render('author')
+  // {"page":{"title":user.name}, "user":user, "posts":posts}
 })
 
 app.listen(port, async function(){
